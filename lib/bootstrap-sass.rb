7b1da93d4aeba65e62d869277d162e1f7c984c21
Steve Jabour <steve@jabour.me>
2015-11-11 15:37:42 -0700

    2.3.2 in a new wrapper.

--- lib/bootstrap-sass.rb
+++ lib/bootstrap-sass.rb
@@ -23,9 +23,9 @@ module Bootstrap
       File.join assets_path, 'stylesheets'
     end
 
-    def fonts_path
-      File.join assets_path, 'fonts'
-    end
+    # def images_path
+    #   File.join assets_path, 'images'
+    # end
 
     def javascripts_path
       File.join assets_path, 'javascripts'
@@ -75,7 +75,7 @@ module Bootstrap
 
     def register_sprockets
       Sprockets.append_path(stylesheets_path)
-      Sprockets.append_path(fonts_path)
+      # Sprockets.append_path(images_path)
       Sprockets.append_path(javascripts_path)
     end
   end
--- lib/bootstrap-sass/version.rb
+++ lib/bootstrap-sass/version.rb
@@ -1,4 +1,4 @@
 module Bootstrap
-  VERSION       = '3.3.5'
-  BOOTSTRAP_SHA = '16b48259a62f576e52c903c476bd42b90ab22482'
+  VERSION       = '2.3.2.3'
+  BOOTSTRAP_SHA = 'b4c4072679943773a7582daa63f545ba818fa627'
 end
